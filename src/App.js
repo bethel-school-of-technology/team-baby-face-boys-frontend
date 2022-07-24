480e4906316acafc21d8563d692ea3e750a0d69c
Brandon Schraven <brandon.schraven@bethel.com>
2022-07-24 15:19:23 -0700

    added routes for screens

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import footer from "./components/footer";
@@ -12,25 +13,24 @@ import forum from './screens/forum';
 import leaderboard from './screens/leaderboard';
 
 function App() {
-  return (
-    <>
-      <header />
-      <Router>
-        <Routes>
-          <Route path="/" element={<titlepage />} />
-          <Route path="/login" element={<signuplogin/>} />
-          <Route path="/registration" element={<registrationpage/>} />
-          <Route path="/profile" element={<profilepage/>} />
-          <Route path="/users12" element={<users12/>} />
-          <Route path="/users13" element={<users13/>} />
-          <Route path="/users13" element={<users13/>} />
-          <Route path="/forum" element={<forum/>} />
-          <Route path="/leaderboard" element={<leaderboard/>} />
-        </Routes>
-      </Router>
-      <footer />
-    </>
-  );
+  // // return (
+  // //   <>
+  // //     <header />
+  // //     <Router>
+  // //       <Routes>
+  // //         <Route path="/" element={<titlepage />} />
+  // //         <Route path="/login" element={<signuplogin/>} />
+  // //         <Route path="/registration" element={<registrationpage/>} />
+  // //         <Route path="/profile" element={<profilepage/>} />
+  // //         <Route path="/users12" element={<users12/>} />
+  // //         <Route path="/users13" element={<users13/>} />
+  // //         <Route path="/forum" element={<forum/>} />
+  // //         <Route path="/leaderboard" element={<leaderboard/>} />
+  // //       </Routes>
+  // //     </Router>
+  // //     <footer />
+  //   </>
+  // );
 }
 
 export default App;
