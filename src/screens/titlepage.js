09066a9e04c7b020938bdeca7cf32917e18513c4
Isaac Martin <isaacamartin@outlook.com>
2022-07-26 23:35:41 -0400

    Titlepage text Centered for header, button text, footer

--- src/screens/titlepage.js
+++ src/screens/titlepage.js
@@ -6,24 +6,23 @@ const Titlepage = () => {
   return (
     <div class="container">
       <div class="row">
-        <div class="col">
+        <div class="col-sm-8">
           <Header />
         </div>
-        <div class="col">
+        <div class="col-sm-4">
           <h1>Right side icons</h1>
         </div>
       </div>
       <div class="row bgnd_img align-items-center">
-        <div class="col">
+        <div class="col text-center">
           <h3>Click the button to enter!</h3>
-          <div class="col">
+          <div class="col ">
             <h1>Title Page Placeholder</h1>
           </div>
         </div>
       </div>
       <div class="row footer">
-        <div class="col">
-          {" "}
+        <div class="col text-center">
           <Footer />
         </div>
       </div>
