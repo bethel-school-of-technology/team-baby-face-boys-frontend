d196436b5047c01cf7419b5979e564972efe2b39
Isaac Martin <isaacamartin@outlook.com>
2022-07-26 22:47:54 -0400

    implemented bootstrap into index.html, Added bootstrap styling to the titlepage.js file and created the design_guidelines.txt

--- src/screens/titlepage.js
+++ src/screens/titlepage.js
@@ -1,15 +1,34 @@
-import React from 'react';
-import Header from '../components/header';
-import Footer from '../components/footer';
+import React from "react";
+import Header from "../components/header";
+import Footer from "../components/footer";
 
 const Titlepage = () => {
-    return (
-        <div>
-            <Header />
-            <h1>Title Page Placeholder</h1>
-            <Footer />
+  return (
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <Header />
         </div>
-    );
+        <div class="col">
+          <h1>Right side icons</h1>
+        </div>
+      </div>
+      <div class="row bgnd_img align-items-center">
+        <div class="col">
+          <h3>Click the button to enter!</h3>
+          <div class="col">
+            <h1>Title Page Placeholder</h1>
+          </div>
+        </div>
+      </div>
+      <div class="row footer">
+        <div class="col">
+          {" "}
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Titlepage;
