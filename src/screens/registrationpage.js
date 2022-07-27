147e0267301991fb2ceafebe647a43b13e3bb2be
Brandon Schraven <brandon.schraven@bethel.com>
2022-07-26 21:45:21 -0700

    added registration form bs

--- src/screens/registrationpage.js
+++ src/screens/registrationpage.js
@@ -6,7 +6,28 @@ const Registrationpage = () => {
     return (
         <div>
             <Header />
-            <h1>Registration Page Placeholder</h1>
+            <div>
+                <div>
+                    <h4>Photo Placeholder</h4>
+                </div>
+                <div>
+                    <h5>Create New User</h5>
+                    <form>
+                        <label> Full Name:</label>
+                        <input type="text" id="fullName" name="fullName" />
+                        <br></br>
+                        <label> DOB:<input type="text" id="dateOfBirth" name="dateOfbirth" /></label>
+                        <br></br>
+                        <label> Gamer ID:<input type="text" id="gamerID" name="gamerID" /></label>
+                        <br></br>
+                        <label> Email:<input type="text" id="firstName" name="email" /></label>
+                        <br></br>
+                        <label> Password:<input type="text" id="password" name="password" /></label>
+                        <br></br>
+                        <input type="submit" value="Submit"/>
+                    </form>
+                </div>
+            </div>
             <Footer />
         </div>
     );
