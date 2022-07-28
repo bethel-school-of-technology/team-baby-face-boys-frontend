2698c0671bee7e0a27583854ecf3672a25ea512e
Brandon Schraven <brandon.schraven@bethel.com>
2022-07-27 20:53:58 -0700

    created functional registration form for registration page bs

--- src/screens/registrationpage.js
+++ src/screens/registrationpage.js
@@ -1,30 +1,41 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import Header from '../components/header';
 import Footer from '../components/footer';
 
 const Registrationpage = () => {
+    const { register, handleSubmit } = useForm();
+    const handleRegistration = (data) => console.log(data);
     return (
         <div>
             <Header />
             <div>
-                <div>
+                <div id="container">
                     <h4>Photo Placeholder</h4>
                 </div>
                 <div>
-                    <h5>Create New User</h5>
-                    <form>
-                        <label> Full Name:</label>
-                        <input type="text" id="fullName" name="fullName" />
-                        <br></br>
-                        <label> DOB:<input type="text" id="dateOfBirth" name="dateOfbirth" /></label>
-                        <br></br>
-                        <label> Gamer ID:<input type="text" id="gamerID" name="gamerID" /></label>
-                        <br></br>
-                        <label> Email:<input type="text" id="firstName" name="email" /></label>
-                        <br></br>
-                        <label> Password:<input type="text" id="password" name="password" /></label>
-                        <br></br>
-                        <input type="submit" value="Submit"/>
+                    <form onSubmit={handleSubmit(handleRegistration)}>
+                        <div>
+                            <label>Full Name: </label>
+                            <input name="fullName" {...register('fullName')} />
+                        </div>
+                        <div>
+                            <label>Gamer ID: </label>
+                            <input name="gamerID" {...register('gamerID')} />
+                        </div>
+                        <div>
+                            <label>Date of Birth: </label>
+                            <input name="DOB" {...register('DOB')} />
+                        </div>
+                        <div>
+                            <label>Email: </label>
+                            <input type="email" name="email" {...register('email')} />
+                        </div>
+                        <div>
+                            <label>Password:</label>
+                            <input type="password" name="password" {...register('password')} />
+                        </div>
+                        <button>Submit</button>
                     </form>
                 </div>
             </div>
