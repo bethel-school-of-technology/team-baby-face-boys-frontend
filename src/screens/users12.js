c21a65bb7bf2cb27b9a72794bec283f4370ef2ba
Isaac Martin <isaacamartin@outlook.com>
2022-08-02 00:24:49 -0400

    Refined game_tile CSS rule and Have made improvements to the gaming tile grid system for the arcade game pages Using proper bootstrap spacing utilities.

--- src/screens/users12.js
+++ src/screens/users12.js
@@ -15,9 +15,14 @@ const Preteen = () => {
           If so, should the game name be in a mostly transparent colored stripe across the top?*/}
       {/* <div className="game_name"> </div>*/}
       <div className="container text-secondary  bgnd_img-12under align-items-center">
+             
         <div className="row">
-          <div className="col-4 " >
-          <div className="game_tile"></div>
+          <div className="col" >
+          <div className="game_tile rounded float-left mt-5 mx-3 "></div>
+          <div className="game_tile rounded float-left mt-5 mx-3 "></div>
+          <div className="game_tile rounded float-left mt-5 mx-3 "></div>
+          <div className="game_tile rounded float-left mt-5 mx-3 "></div>
+          <div className="game_tile rounded float-left mt-5 mx-3 "></div>
           </div>
         </div>
       </div>
