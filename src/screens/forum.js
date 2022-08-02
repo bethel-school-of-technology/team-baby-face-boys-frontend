100a44202f27ffac9254858b554d3815227ffed2
Zach Carlson <95587273+Sylverr38@users.noreply.github.com>
2022-08-02 18:24:59 -0500

    Beginnings of Forum Stuff

--- src/screens/forum.js
+++ src/screens/forum.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
+import { useState } from 'react';
+
+function Blogboard() {
+    const [blog, setBlog] = useState("");
+}
+
+const displayBlog = () => {
+    console.log(blog);
+};
 
 const Forum = () => {
     return (
@@ -8,8 +17,11 @@ const Forum = () => {
              <Navbar />
             <h1>Rabbit Water Cooler</h1>
             <h4>Create Post:</h4>
-            <h4>Rabbit Users:</h4>
+            <input type="text" onchange={(event) => {setBlog(event.target.value);
+            }}/>
+            <button onClick={displayBlog}>Submit Post</button>
             <h4>Forum Posts:</h4>
+            <h4>Rabbit Users:</h4>
            <Footer />
         </div>
 
