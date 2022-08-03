491477a8a412b4a8cca54625c9ae8f17f09567b1
Isaac Martin <isaacamartin@outlook.com>
2022-08-03 16:53:05 -0400

    Game pages are completely responsive friendly and nearly complete. The leaderboard page is styled and formatted. The forum pages looking pretty good as well, but lacks content for now.

--- src/screens/leaderboard.js
+++ src/screens/leaderboard.js
@@ -1,16 +1,56 @@
-import React from 'react';
-import Navbar from '../components/navbar';
-import Footer from '../components/footer';
+import React from "react";
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
 
 const Leaderboard = () => {
-    return (
-        <div class="container">
-            <Navbar />
-            <h1>Top Rabbit Leaderboard</h1>
-            <Footer />
+  return (
+    <div className="container-fluid  d-flex flex-column">
+      <div className="row">
+        <div className="col-sm p-3 text-center">
+          <Navbar />
         </div>
-    );
+      </div>
+      <div className="container  scoreboard align-items-center"><div className="table_holder mx-auto" >
+      <table class="table table-striped">
+    <thead>
+        
+      <tr>
+        <th>Username</th>
+        <th>Game</th>
+        <th>Hi score</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>fripperybonkers</td>
+        <td>Whack-A-Mole</td>
+        <td>863</td>
+      </tr>
+      <tr>
+        <td>doodadjumbo</td>
+        <td>Memory</td>
+        <td>185</td>
+      </tr>
+      <tr>
+        <td>poofmumu</td>
+        <td>Breakout</td>
+        <td>2,773</td>
+      </tr>
+      <tr>
+        <td>huzzahmeow</td>
+        <td>Connect 4</td>
+        <td>Fifty Straight Wins!</td>
+      </tr>
+    </tbody>
+  </table></div>
+      </div>
+      <div className="row footer">
+        <div className="col text-center p-3">
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Leaderboard;
-
--- src/screens/users13.js
+++ src/screens/users13.js
@@ -1,46 +1,50 @@
-import React from 'react';
-import Navbar from '../components/navbar';
-import Footer from '../components/footer';
+import React from "react";
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
 
 const Teen = () => {
-    return (<div className="container-fluid  d-flex flex-column">
-    <div className="row">
-      <div className="col-sm p-3 text-center">
-        <Navbar />
-      </div>
-    </div>
-    {/* Should the game go in a frame with the game Name above it or
-        should the game name be inside the frame with the game?
-        If so, should the game name be in a mostly transparent colored stripe across the top?*/}
-    {/* <div className="game_name"> </div>*/}
-    <div className="container text-secondary  bgnd_img-12under align-items-center">
-           
+  return (
+    <div className="container-fluid  d-flex flex-column">
       <div className="row">
-        <div className="col img_grid" >
-        {/* <div className="game_snap_1 rounded float-left mt-5 mx-3 "></div> */}
-        <img className="game_thumb img-fluid rounded float-left mt-5 mx-3" src={ require('../images/game_thumbs/breakout.jpg') } />
-        <img className="game_thumb img-fluid rounded float-left mt-5 mx-3" src={ require('../images/game_thumbs/cnnct4.jpg') } />
-        <img className="game_thumb img-fluid rounded float-left mt-5 mx-3" src={ require('../images/game_thumbs/memory.jpg') } />
-        <img className="game_thumb img-fluid rounded float-left mt-5 mx-3" src={ require('../images/game_thumbs/rockpaperscissors.jpg') } />
-        <img className="game_thumb img-fluid rounded float-left mt-5 mx-3" src={ require('../images/game_thumbs/whackamole.jpg') } />
-        
-        
-        {/* 
-        <div className="game_snap_2 rounded float-left mt-5 mx-3 "></div>
-        <div className="game_snap_3 rounded float-left mt-5 mx-3 "></div>
-        <div className="game_snap_4 rounded float-left mt-5 mx-3 "></div>
-        <div className="game_snap_5 rounded float-left mt-5 mx-3 "></div> */}
-        
+        <div className="col-sm p-3 text-center">
+          <Navbar />
+        </div>
+      </div>
+      {/* the game name Should be in a mostly transparent colored stripe across the top?*/}
+      {/* <div className="game_name"> </div>*/}
+      <div className="container text-secondary  bgnd_img-12under align-items-center">
+        <div className="row">
+          <div className="col img_grid">
+            <img
+              className="game_thumb img-fluid rounded float-left mt-5 mx-3"
+              src={require("../images/game_thumbs/breakout.jpg")}
+            />
+            <img
+              className="game_thumb img-fluid rounded float-left mt-5 mx-3"
+              src={require("../images/game_thumbs/cnnct4.jpg")}
+            />
+            <img
+              className="game_thumb img-fluid rounded float-left mt-5 mx-3"
+              src={require("../images/game_thumbs/memory.jpg")}
+            />
+            <img
+              className="game_thumb img-fluid rounded float-left mt-5 mx-3"
+              src={require("../images/game_thumbs/rockpaperscissors.jpg")}
+            />
+            <img
+              className="game_thumb img-fluid rounded float-left mt-5 mx-3"
+              src={require("../images/game_thumbs/whackamole.jpg")}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="row footer">
+        <div className="col text-center p-3">
+          <Footer />
         </div>
       </div>
     </div>
-    <div className="row footer">
-      <div className="col text-center p-3">
-        <Footer />
-      </div>
-    </div>
-  </div>
-    );
+  );
 };
 
 export default Teen;
