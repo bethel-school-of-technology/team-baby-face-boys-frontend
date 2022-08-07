07ee0ce516d98d3bcc00a7d91c42ad118738f54e
Isaac Martin <isaacamartin@outlook.com>
2022-08-07 02:21:04 -0400

    The following pages have been completed: sign up page. login page, And numerous sitewide improvements to the interface and too many elements to list. Work has begun on the dad joke API

--- src/screens/titlepage.js
+++ src/screens/titlepage.js
@@ -1,28 +1,32 @@
 import React from "react";
 import Header from "../components/header";
 import Footer from "../components/footer";
+import Dadjokes from "../components/dadjokes";
+
 import { Link } from "react-router-dom";
 
 const Titlepage = () => {
   return (
-    <div class="container-fluid  bg-primary d-flex flex-column">
-    <div class="row">
-      <div class="col-sm p-3 text-center">
+    <div className="container-fluid  bg-primary d-flex flex-column">
+    <div className="row">
+      <div className="col-sm p-3 text-center">
         <Header/>
       </div>{/* 
-      <div class="col-sm-4 p-3">
+      <div className="col-sm-4 p-3">
         <p>Right side icons?</p>
       </div> */}
     </div>
-    <div class="row text-secondary bgnd_img-ttlpg align-items-center ">
-         <div class="col text-center">
+    <div className="row text-secondary bgnd_img-ttlpg align-items-center ">
+         <div className="col text-center">
         <h3><strong>Click The Logo To Enter!</strong></h3> <br></br>
-        <Link to='/account'><div class="car_btn  mx-auto d-block" ></div></Link>
+        <Link to='/account'><div className="car_btn  mx-auto d-block" ></div></Link>
       </div>
   </div>
-    <div class="row footer">
-      <div class="col text-center p-3">
+    <div className="row footer">
+      <div className="col text-center p-3">
+      <script src="./" ></script>
         <Footer/>
+        <Dadjokes/>
         </div>
     </div>
   </div>
