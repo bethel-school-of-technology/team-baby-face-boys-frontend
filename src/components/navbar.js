549089e8a04254cd5de8b6d5d063f47d705589cc
Isaac Martin <isaacamartin@outlook.com>
2022-08-10 17:08:46 -0400

    Navbar Has been fixed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,17 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-    return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary  ">
-        <a class="navbar-brand" href="/"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-      
-        <div class="collapsenavbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto text-warning ">
-            <li class="nav-item active">
-              <a class="nav-link" href="/profile">Home <span class="sr-only">(current)</span></a>
+    return (<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse" id="navbarColor02">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active"><a class="nav-link" href="/profile">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="/allgames">Arcade</a>
@@ -22,10 +20,10 @@ const Navbar = () => {
               </li>
             <li class="nav-item">
               <a class="nav-link" href="/leaderboard">Leaderboard</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+              </li>
+        </ul>
+    </div>
+  </nav>
     );
 };
 
