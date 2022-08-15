3b6ccc90e2c4a8bf35fc24e9e2cf9e81bf072581
Zach Carlson <95587273+Sylverr38@users.noreply.github.com>
2022-08-14 21:48:59 -0500

    Added Preliminary JS Game Code Take 2 ZC

--- src/games/memoryModal.js
+++ src/games/memoryModal.js
@@ -1,19 +1,141 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import "./memory.css"
+
+function MemoryModal({ closeMemory }) {
 
 
-function MemoryModal({closeMemory}) {
+    const cardArray = [
+        {
+            name: 'fries',
+            img: 'gamePictures/fries.png',
+        },
+        {
+            name: 'cheeseburger',
+            img: 'gamePictures/cheeseburger.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'gamePictures/hotdog.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'gamePictures/ice-cream.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'gamePictures/milkshake.png'
+        },
+        {
+            name: 'pizza',
+            img: 'gamePictures/pizza.png'
+        },
+        {
+            name: 'fries',
+            img: 'gamePictures/fries.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'gamePictures/cheeseburger.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'gamePictures/hotdog.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'gamePictures/ice-cream.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'gamePictures/milkshake.png'
+        },
+        {
+            name: 'pizza',
+            img: 'gamePictures/pizza.png'
+        }
+    ]
+    
+    cardArray.sort(() => 0.5 - Math.random())
+    
+    const gridDisplay = document.querySelector('#grid')
+    const resultDisplay = document.querySelector('#result')
+    let cardsChosen = []
+    let cardsChosenIds = []
+    const cardsWon = []
+    
+    function createBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+            const card = document.createElement('img')
+            card.setAttribute('src', 'images/blank.png')
+            card.setAttribute('data-id', i)
+            card.addEventListener('click', flipCard)
+            gridDisplay.append(card)
+        }
+    }
+    
+    createBoard()
+    
+    function checkMatch() {
+        const cards = document.querySelectorAll('img')
+        const optionOneId = cardsChosenIds[0]
+        const optionTwoId = cardsChosenIds[1]
+        if (optionOneId == optionTwoId) {
+            alert('You have clicked the same card!')
+            cards[optionOneId].setAttribute('src', 'images/blank.png')
+            cards[optionTwoId].setAttribute('src', 'images/blank.png')
+        }
+        if (cardsChosen[0] == cardsChosen[1]) {
+            alert('You found a match!')
+            cards[optionOneId].setAttribute('src', 'images/white.png')
+            cards[optionTwoId].setAttribute('src', 'images/white.png')
+            cards[optionOneId].removeEventListener('click', flipCard)
+            cards[optionTwoId].removeEventListener('click', flipCard)
+            cardsWon.push(cardsChosen)
+        } else {
+            cards[optionOneId].setAttribute('src', 'images/blank.png')
+            cards[optionTwoId].setAttribute('src', 'images/blank.png')
+            alert("Sorry, try again!")
+        }
+        resultDisplay.textContent = cardsWon.length
+        cardsChosen = []
+        cardsChosenIds = []
+    
+        if (cardsWon.length == cardArray.length / 2) {
+            resultDisplay.innerHTML = "Congratulations! You found them all! Refresh the page to play again!"
+        }
+    }
+    
+    function flipCard() {
+        const cardId = this.getAttribute('data-id')
+        cardsChosen.push(cardArray[cardId].name)
+        cardsChosenIds.push(cardId)
+        console.log(cardsChosen)
+        console.log(cardsChosenIds)
+        this.setAttribute('src', cardArray[cardId].img)
+        if (cardsChosen.length === 2) {
+            setTimeout(checkMatch, 500)
+        }
+    }
+
+
+
+
+
+
+
+
     return (
         <div className="modalBackground">
             <div className="modalContainer">
                 <button onClick={() => closeMemory(false)}>X</button>
-                <div className="modalHeader">
-                    <h3>Memory</h3>
-                </div>
-                <div className="modalBody">
-                    <p>Memory Game Placeholder</p>
-                </div>
-                <div className="modalFooter">
-                </div>
+                <h1 className='memory'>Memory Game</h1>
+               
+                    <div id="game">
+                        <div id="grid"></div>
+                    </div>
+                    <div id="score">
+                        <h3 className='memory'>Score: <span id="result"></span></h3>
+                    </div>
             </div>
         </div>
     );
